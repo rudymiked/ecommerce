d4ce70b2f607f55739ce91388ae48bc41b47ddb7
Mike Rudy <rudymiked@gmail.com>
2015-04-14 21:50:12 -0400

    created more pages

---
+++ products.php
@@ -0,0 +1,47 @@
+<html>
+  <head>
+    <title>
+      Mike Rudy E-Commerece Website
+    </title>
+  </head>
+
+  <div class="menu">
+    <ul id="nav">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Products</a></li>
+      <li><a href="#">Promotions</a></li>
+      <li><a href="#">Information</a></li>
+    </ul>
+  </div>
+
+  <?php
+
+
+
+
+
+
+  ?>
+
+  <table>
+    <tr>
+      <td>
+      Toys
+      </td>
+      <td>
+      Dinosaur
+      </td>
+    </tr>
+    <tr>
+      <td>
+      Games
+      </td>
+      <td>
+      Halo
+      </td>
+    </tr>
+  </table>
+
+
+
+</html>
---
+++ promotions.php
@@ -0,0 +1,29 @@
+<html>
+  <head>
+    <title>
+      Mike Rudy E-Commerece Website
+    </title>
+  </head>
+
+  <div class="menu">
+    <ul id="nav">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Products</a></li>
+      <li><a href="#">Promotions</a></li>
+      <li><a href="#">Information</a></li>
+    </ul>
+  </div>
+
+  <?php
+
+
+
+
+
+
+  ?>
+
+
+
+
+</html>
